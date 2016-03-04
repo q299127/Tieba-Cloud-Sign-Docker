6031be7f2870b0a18ed6d31bb1740ef711a0e962
ubuntu-ssh-timezone <sanzuwu@gmail.com>
2016-03-04 12:58:01 +0800

    Update runcron.sh

--- runcron.sh
+++ runcron.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-#启动cron计划
-echo "===>start cron====>"
+#sanzuwu@gamil.com
+#写入计划到/etc/crontab
+#每天凌晨2点17到2点26、下午3点17到3点26每分钟运行一次do.php
+for i in {17..26};
+do
+  echo "${i} 2 * * * root php /data/http/do.php " >> /etc/crontab;
+  echo "${i} 15 * * * root php /data/http/do.php " >> /etc/crontab;
+done；
+
+#启动cron
+echo "====================================>starting cron<===================================="
 cron start
-echo "done ====>"
+echo "====================================>done!<===================================="
+
